31a8100cc7ffd4d4536569af55049464bd672395
Aurore Rey <rey.pandora@gmail.com>
2021-03-09 17:14:14 +0100

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Broom.destroy_all
+User.destroy_all
+
+User.create!(nickname:"Annie", email:"annie@witch.com", password:"annieannie")
+User.create(nickname:"Mireille", email:"mireille@witch.com", password:"mireillemireille" )
+User.create(nickname:"Rosie", email:"rosie@witch.com", password:"rosierosie")
+User.create(nickname:"Chloé", email: "chloe@witch.com", password:"chloechloe")
+User.create(nickname:"Berthe", email:"berthe@witch.com", password:"bertheberthe")
+User.create(nickname:"Joséphine", email: "joséphine@witch.com", password:"joséphinejoséphine")
+
+users = User.all
+
+Broom.create!(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+Broom.create(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+Broom.create(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+Broom.create(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+Broom.create(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+Broom.create(name: "l'intrépide", description: "Il ne craint pas l'aventure", price: 300,
+  power: 45, twigs: 657, age:"34", user: users.sample)
+
+
+
+
+
+
+
+
+
