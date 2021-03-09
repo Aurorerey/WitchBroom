7fdbe9569b619f1ee427dc2164fa181252de22fd
Josephine Dujardin <josephine83230@gmail.com>
2021-03-09 17:40:59 +0100

    modifications pages

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-$('.navTrigger').click(function () {
-    $(this).toggleClass('active');
-    console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
-
-});
