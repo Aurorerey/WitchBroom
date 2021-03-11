f703b9a86468eb1cbe1f9a7b78523685159fc5da
Ludovic Jouenne <goldeneagles@protonmail.com>
2021-03-11 18:09:10 +0100

    Ajout simple de la map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,62 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import mapboxgl from 'mapbox-gl';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+// const injectCoordinates = (coordinates) => {
+//   const paragraphElement = document.getElementById('coordinates');
+//   paragraphElement.innerText = `${coordinates.lat}, ${coordinates.lng}`;
+// };
+
+// const injectMap = (coordinates) => {
+//   const lng = coordinates.lng;
+//   const lat = coordinates.lat;
+
+  const mapDiv = document.getElementById('map');
+  mapDiv.innerHTML = '';
+
+//   mapboxgl.accessToken = 'pk.eyJ1IjoiZ29sZGVuZWFnbGVzMzMzIiwiYSI6ImNrbGtwcWNuMTI0ZmgydnByanQwYzJtYWMifQ.m5n7IkJitqxnmwfcfHTLAw';
+//   const map = new mapboxgl.Map({
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/streets-v9',
+//     center: [lng, lat],
+//     zoom: 12
+//   });
+//   new mapboxgl.Marker()
+//     .setLngLat([lng, lat])
+//     .addTo(map);
+// };
+
+// const formElement = document.getElementById('searchForm');
+// Window.addEventListener('DOMContentLoaded', (event) => {
+  // prevent form's default behavior
+  // event.preventDefault();
+
+  // get input value
+  // const address = document.getElementById('address').value;
+  const mapboxKey = 'pk.eyJ1IjoiZ29sZGVuZWFnbGVzMzMzIiwiYSI6ImNrbGtwcWNuMTI0ZmgydnByanQwYzJtYWMifQ.m5n7IkJitqxnmwfcfHTLAw';
+  // trigger request to Google Maps API
+  fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/marseille.json?access_token=${mapboxKey}`)
+    .then(response => response.json())
+    .then((data) => {
+      // const coordinates = {
+      //   lng: data.features[0].geometry.coordinates[0],
+      //   lat: data.features[0].geometry.coordinates[1]
+      // };
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZ29sZGVuZWFnbGVzMzMzIiwiYSI6ImNrbGtwcWNuMTI0ZmgydnByanQwYzJtYWMifQ.m5n7IkJitqxnmwfcfHTLAw';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1]],
+    zoom: 12
+  });
+//   new mapboxgl.Marker()
+//     .setLngLat([lng, lat])
+//     .addTo(map);
+});
 });
